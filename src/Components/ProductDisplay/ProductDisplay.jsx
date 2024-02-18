fbae45c260d62274d7cc8b222e151c644bd01d8a
Adarshnair07 <adarshnair0703@gmail.com>
2024-02-19 00:25:27 +0530

    newprjct

---
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,61 @@
+import React, { useContext } from "react";
+import "./ProductDisplay.css";
+import star_icon from "../Assets/star_icon.png";
+import star_dull_icon from "../Assets/star_dull_icon.png";
+import { ShopContext } from "../../Context/ShopContext";
+
+const ProductDisplay = (props) => {
+
+  const {product} = props;
+  const {addToCart} = useContext(ShopContext);
+
+  return (
+    <div className="productdisplay">
+      <div className="productdisplay-left">
+        <div className="productdisplay-img-list">
+          <img src={product.image} alt="img" />
+          <img src={product.image} alt="img" />
+          <img src={product.image} alt="img" />
+          <img src={product.image} alt="img" />
+        </div>
+        <div className="productdisplay-img">
+          <img className="productdisplay-main-img" src={product.image} alt="img" />
+        </div>
+      </div>
+      <div className="productdisplay-right">
+        <h1>{product.name}</h1>
+        <div className="productdisplay-right-stars">
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_dull_icon} alt="" />
+          <p>(122)</p>
+        </div>
+        <div className="productdisplay-right-prices">
+          <div className="productdisplay-right-price-old">₹{product.old_price}</div>
+          <div className="productdisplay-right-price-new">₹{product.new_price}</div>
+        </div>
+        <div className="productdisplay-right-description">
+        The Air Jordan 1 Mid SE 'Black/Gold' is making a comeback in November 2023.
+         This colourway was first revealed in 2018 and will be available in full family sizes
+        </div>
+        <div className="productdisplay-right-size">
+          <h1>Select Size (UK)</h1>
+          <div className="productdisplay-right-sizes">
+            <div>5</div>
+            <div>6</div>
+            <div>7</div>
+            <div>8</div>
+            <div>9</div>
+          </div>
+        </div>
+        <button onClick={()=>{addToCart(product.id)}}>ADD TO CART</button>
+        <p className="productdisplay-right-category"><span>Category :</span> Sneakers, Trainers</p>
+        <p className="productdisplay-right-category"><span>Tags :</span> Modern, Latest</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDisplay;
